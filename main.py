bca855e067b802a3e09a7fe88a67b7fa656ea404
Fima3078a <fima.semenow@gmail.com>
2024-12-15 14:04:14 +0300

    Update main.py
    1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+import tkinter as tk
+
+win = tk.Tk()
+canvas = tk.Canvas(win, bg="red", height=400, width=400)
+for i in range(50, 400, 50):
+    canvas.create_line((0,i), (400, i), fill='black')
+    canvas.create_line((i, 0), (i, 400), fill='black')
+canvas.pack()
+canvas.mainloop()
